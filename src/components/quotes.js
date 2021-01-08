425784dcf59e5399a5530b9b1799a88cd8bf8ede
Anikesh99 <b.anikesh@gmail.com>
2021-01-09 01:44:39 +0800

    Add functionality to UI

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -2,51 +2,80 @@ import React, {useState, useEffect} from 'react'
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Slider from "@material-ui/core/Slider";
 import Backdrop from "@material-ui/core/Backdrop";
 import clsx from "clsx"
-import SettingsIcon from "@material-ui/icons/Settings";
-import Tooltip from "@material-ui/core/Tooltip";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
 import Grid from "@material-ui/core/Grid";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Collapse from "@material-ui/core/Collapse";
 import axios from "axios";
 import Typography from "@material-ui/core/Typography";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import Radio from "@material-ui/core/Radio";
 import { green } from "@material-ui/core/colors";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
 import IconButton from "@material-ui/core/IconButton";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormLabel from "@material-ui/core/FormLabel";
 import ShuffleIcon from "@material-ui/icons/Shuffle";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 
+const logo = require('./icon.png');
 
-const api = "https://fastapi-quote-l6lhxur2aq-uc.a.run.app/"
-const LargeTooltip = withStyles((theme) => ({
-    tooltip: {
-      //   backgroundColor: theme.palette.common.white,
-      //   color: 'rgba(0, 0, 0, 0.87)',
-      boxShadow: theme.shadows[1],
-      fontSize: 14,
+const iOSBoxShadow =
+  '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+
+const LengthSlider = withStyles({
+  root: {
+    color: '#3880ff',
+    height: 2,
+    padding: '15px 0',
+  },
+  thumb: {
+    height: 28,
+    width: 28,
+    backgroundColor: '#fff',
+    boxShadow: iOSBoxShadow,
+    marginTop: -14,
+    marginLeft: -14,
+    '&:focus, &:hover, &$active': {
+      boxShadow: '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+      // Reset on touch devices, it doesn't add specificity
+      '@media (hover: none)': {
+        boxShadow: iOSBoxShadow,
+      },
     },
-  }))(Tooltip);
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 12px)',
+    top: -22,
+    '& *': {
+      background: 'transparent',
+      color: '#000',
+    },
+  },
+  track: {
+    height: 2,
+  },
+  rail: {
+    height: 2,
+    opacity: 0.5,
+    backgroundColor: '#bfbfbf',
+  },
+  mark: {
+    backgroundColor: '#bfbfbf',
+    height: 8,
+    width: 1,
+    marginTop: -3,
+  },
+  markActive: {
+    opacity: 1,
+    backgroundColor: 'currentColor',
+  },
+})(Slider);
   
   const useStyles = makeStyles((theme) => ({
     root: {
-      height: "100vh",
+      height: "200vh",
     },
     image: {
       backgroundRepeat: "no-repeat",
@@ -55,20 +84,19 @@ const LargeTooltip = withStyles((theme) => ({
           ? theme.palette.grey[50]
           : theme.palette.grey[900],
       backgroundSize: "cover",
-      backgroundPosition: "center",
+      backgroundPosition: "fill",
+      height: "71.6%",
     },
     paper: {
       margin: theme.spacing(8, 4),
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
+      height: "15%"
     },
     avatar: {
       margin: theme.spacing(1),
       backgroundColor: theme.palette.secondary.main,
     },
     form: {
-      width: "100%", // Fix IE 11 issue.
+      width: "100%", 
       marginTop: theme.spacing(5),
     },
     submit: {
@@ -81,11 +109,12 @@ const LargeTooltip = withStyles((theme) => ({
       width: "100%",
     },
     textField: {
+      width: "100%"
       // width: '25ch',
     },
     slider: {
       marginTop: "60px",
-      width: "90%",
+      width: "100%",
       // boxSizing: "border-box",
     },
     sliderLabel: {
@@ -99,15 +128,10 @@ const LargeTooltip = withStyles((theme) => ({
       marginTop: -12,
       marginLeft: -12,
     },
-    starRating: {
-      position: "absolute",
-      top: "85%",
-      background: ":rgba(0,0,0,0.6)",
-      borderRadius: "5px",
-    },
     backdrop: {
       zIndex: theme.zIndex.drawer + 1,
       color: "#fff",
+      width: "100%"
     },
     progressModel: {
       width: "50%",
@@ -124,68 +148,67 @@ const LargeTooltip = withStyles((theme) => ({
 function QuoteGenerator() {
     const unsplashApi = "https://api.unsplash.com/photos/random";
 
-    const persona = {
+    const model = {
         serious: {
           name: "AristoBot",
-          description:
-            "I am a wise old fool who ponders about Philosophy, Religion, Life and Death to uncover deep truths of the universe.",
-          image_search: ["universe", "life", "philosophy"],
+          image_search: ['inspiration', 'motivation', 'life-lesson', 'love', 'hope', 'friendship', 'life', 'faith', 'universe', 'nature'],
         }
     };
 
     const keys = [];
-    for (var k in persona){
+    for (var k in model){
       keys.push(k)
     }
     const classes = useStyles();
     const [modelName, setModelName] = React.useState(
         keys[Math.floor(Math.random() * keys.length)]
     );
-    const [nextSeed, setNextSeed] = React.useState("");
+    const [nextSeed, setNextSeed] = React.useState(" ");
     const [genSeed, setGenSeed] = React.useState("");
     const [bgImage, setBgImage] = useState("")
     const [quote, setQuote] = useState("")
     const [author, setAuthor] = useState("")
     const [minLength, setMinLength] = React.useState(10);
-    const [maxLength, setMaxLength] = React.useState(100);
+    const [maxLength, setMaxLength] = React.useState(50);
     const [modelLoading, setModelLoading] = React.useState(false);
     const [loadingSeed, setLoadingSeed] = React.useState(false);
     const [generatingQuote, setGeneratingQuote] = React.useState(false);
     const [open, setOpen] = React.useState(false);
 
     const handleSubmit = (event) => {
+      setGeneratingQuote(true)
       setLoadingSeed(true);
       getRandomImage();  
       setQuote("");
         setAuthor("");
         axios
-          .get("https://fastapi-quoter2-l6lhxur2aq-uc.a.run.app/gen/" + nextSeed, {
-              start_text: nextSeed
-            }
+          .get("https://quotify-engine-l6lhxur2aq-uc.a.run.app/generate/" + nextSeed + "/" + minLength + "/" + maxLength + "/0.5"
           )
           .then(function (response) {
             console.log("Response in submit: " + response["data"]["output"]);
             setQuote(response["data"]["output"]);
-            setAuthor(persona[modelName]["name"]);
+            setAuthor("Quotify");
             setGeneratingQuote(false);
           })
           .catch((error) => {
-            console.log(error.response);
+            console.log(error);
             setQuote("Uh Oh! Something's wrong. Please come back later");
-            setAuthor("Quote Father");
+            setAuthor("Quotify");
             setGeneratingQuote(false);
           });
           setLoadingSeed(false);
+          setGeneratingQuote(false)
         event.preventDefault();
     }
+
     const getRandomImage = () => {
         axios
           .get(unsplashApi, {
             params: {
               query:
-                persona[modelName]["image_search"][
+                model["serious"]["image_search"][
                   Math.floor(
-                    Math.random() * persona[modelName]["image_search"].length
+                    Math.random() * model["serious"]["image_search"].length 
                   )
                 ],
               featured: true,
@@ -205,14 +228,13 @@ function QuoteGenerator() {
 
       useEffect(() => {
         setGeneratingQuote(true);
+        setLoadingSeed(true)
         getRandomImage();
-            axios
-              .get("https://fastapi-quoter2-l6lhxur2aq-uc.a.run.app/gen/" + nextSeed, {
-                  start_text: ""
-              })
+        axios
+          .get("https://quotify-engine-l6lhxur2aq-uc.a.run.app/generate/" + nextSeed + "/" + minLength + "/" + maxLength + "/0.5/")
               .then(function (response) {
                 setQuote(response["data"]["output"]);
-                setAuthor(persona[modelName]["name"]);
+                setAuthor("Quotify");
                 setGeneratingQuote(false);
                 setLoadingSeed(false);
               })
@@ -220,7 +242,7 @@ function QuoteGenerator() {
                 console.log(error);
                 setQuote("Uh Oh! Something's wrong. Please come back later");
                 setGenSeed("");
-                setAuthor("AI Bot");
+                setAuthor("Quotify");
                 setGeneratingQuote(false);
                 setLoadingSeed(false);
               });
@@ -234,74 +256,66 @@ function QuoteGenerator() {
         setMaxLength(newValue);
       };
 
+      const handleOpen = () => {
+        setOpen(!open);
+      };
+
       const handleSeedChange = (event) => {
         setNextSeed(event.target.value);
       };
 
       return (
-        <Grid container component="main" className={classes.root}>
+        <Grid container component="main" style = {{flexWrap:"wrap", width: "100%", height:"100vh"}} justify= 'space-around' direction = "column" alignItems="stretch">
         <CssBaseline />
-        {/* style={{background: "#f5f5f5"}} */}
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Grid
+          item
+          container
+          direction="column"
+          justify="center"
+          alignItems="stretch"
+          position="relative"
+          className={classes.image}
+          style={{ backgroundImage: "url(" + bgImage + ")" }}
+        >
+          <section>
+            <div class="quote" style = {{textAlign: "center", alignItems:"center", backgroundColor:"black", opacity:"0.6"}}>
+              <hr class="line" />
+              {generatingQuote ? (
+                <div>
+                  <Typography
+                    style={{ marginLeft: "10px", color: "white" }}
+                    // variant="overline"
+                  >
+                    Generating quote...
+                  </Typography>
+                  <CircularProgress
+                    color="secondary"
+                    className={classes.progressModel}
+                  />
+                </div>
+              ) : (
+                <p class="text" style={{opacity:"100%", color:"white", fontSize: "36px" }}>{quote}</p>
+              )}
+              <p class="attr" style={{alignText: "right",  color:"white", fontSize: "30px"}}>- Quotify</p>
+              {/* <hr class="line" /> */}
+            </div>
+          </section>
+        </Grid>
+        <Grid item 
+        className={classes.paper}
+        >
           <Backdrop className={classes.backdrop} open={modelLoading}>
             <Typography variant="overline">Fetching {modelName}...</Typography>
             <LinearProgress className={classes.progressModel} color="primary" />
           </Backdrop>
-          <div className={classes.paper}>
-            <img src="../logo.png" style={{ width: "60%" }} alt="AI Quotes Generator"/>
-            <form className={classes.form} noValidate onSubmit={handleSubmit}>
-              <Grid container justify="center">
-                <FormControl variant="outlined" className={classes.formControl}>
-                  <FormLabel component="legend">Select the Persona</FormLabel>
-                  <RadioGroup
-                    row
-                    aria-label="position"
-                    name="position"
-                    value={modelName}
-                    // onChange={handleModelNameChange}
-                  >
-                    <LargeTooltip
-                      title={persona["serious"]["description"]}
-                      placement="top"
-                    >
-                      <FormControlLabel
-                        value="serious"
-                        control={<Radio color="primary" />}
-                        label={persona["serious"]["name"]}
-                        labelPlacement="end"
-                      />
-                    </LargeTooltip>
-                    {/* <LargeTooltip
-                      title={persona["serious"]["description"]}
-                      placement="top"
-                    >
-                      <FormControlLabel
-                        value="serious"
-                        control={<Radio color="primary" />}
-                        label={persona["serious"]["name"]}
-                        labelPlacement="end"
-                      />
-                    </LargeTooltip>
-                    <LargeTooltip
-                      title={persona["funny"]["description"]}
-                      placement="top"
-                    >
-                      <FormControlLabel
-                        value="funny"
-                        control={<Radio color="primary" />}
-                        label={persona["funny"]["name"]}
-                        labelPlacement="end"
-                      />
-                    </LargeTooltip> */}
-                  </RadioGroup>
-                </FormControl>
-              </Grid>
-              <FormControl
-                fullWidth
-                className={clsx(classes.margin, classes.textField)}
-              >
-                <InputLabel htmlFor="seed" style={{ marginLeft: "2%" }}>
-                  Start off the quote or leave it empty to go completely random
+          <div >
+            <img src={logo}  alt="Quotify"/>
+            <div style ={{"display": "flex", width:"100%"}}>
+              
+            <form className={classes.form} noValidate onSubmit={handleSubmit}> 
+            <FormControl fullWidth className={clsx(classes.margin, classes.textField)}>
+                <InputLabel htmlFor="seed" style={{fontSize:"20px", marginLeft: "1%", marginTop: "1%"}}>
+                  Leave a phrase and let Quotify blow your mind
                 </InputLabel>
                 <OutlinedInput
                   id="seed"
@@ -324,36 +338,9 @@ function QuoteGenerator() {
                     </InputAdornment>
                   }
                 />
-              </FormControl>
-              <FormControl fullWidth className={clsx(classes.margin)}>
-                <List>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <SettingsIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Advanced Settings" />
-                    {open ? <ExpandLess /> : <ExpandMore />}
-                  </ListItem>
-                  <Collapse in={open} timeout="auto" unmountOnExit>
-                    
-                    {/* <FormControl className={clsx(classes.margin, classes.slider)}>
-                      <InputLabel
-                        htmlFor="variety"
-                        className={clsx(classes.sliderLabel)}
-                      >
-                        Variety
-                      </InputLabel>
-                      <Slider
-                        id="variety"
-                        value={variety}
-                        onChange={handleVariety}
-                        step={0.1}
-                        marks
-                        min={0.1}
-                        max={0.9}
-                        valueLabelDisplay="on"
-                      />
-                    </FormControl> */}
+                </FormControl>
+              <FormControl fullWidth>
+                
                     <FormControl
                       fullWidth
                       className={clsx(classes.margin, classes.slider)}
@@ -364,17 +351,18 @@ function QuoteGenerator() {
                       >
                         Minimum Number of Words
                       </InputLabel>
-                      <Slider
-                        id="min-length"
-                        value={minLength}
-                        onChange={handleMinLength}
-                        step={10}
-                        marks
-                        min={10}
-                        max={100}
-                        valueLabelDisplay="on"
-                      />
+                      <LengthSlider 
+                      id = 'min-length'
+                      aria-label="Minimum Number of Words" 
+                      onChange={handleMinLength} 
+                      value={minLength}
+                      valueLabelDisplay="on" 
+                      step={10}
+                      marks
+                      min={10}
+                      max={100}/>
                     </FormControl>
+                    <br/>
                     <FormControl
                       fullWidth
                       className={clsx(classes.margin, classes.slider)}
@@ -385,8 +373,9 @@ function QuoteGenerator() {
                       >
                         Maximum Number of Words
                       </InputLabel>
-                      <Slider
+                      <LengthSlider
                         id="max-length"
+                        aria-label="Maximum Number of Words"
                         value={maxLength}
                         onChange={handleMaxLength}
                         step={10}
@@ -396,10 +385,10 @@ function QuoteGenerator() {
                         valueLabelDisplay="on"
                       />
                     </FormControl>
-                  </Collapse>
-                </List>
               </FormControl>
-              <FormControl fullWidth className={clsx(classes.margin)}>
+              <FormControl fullWidth 
+              // className={clsx(classes.margin)}
+              >
                 <Button
                   type="submit"
                   fullWidth
@@ -418,42 +407,8 @@ function QuoteGenerator() {
                 )}
               </FormControl>
             </form>
-          </div>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={4}
-          md={7}
-          className={classes.image}
-          style={{ backgroundImage: "url(" + bgImage + ")" }}
-        >
-          <section>
-            {/* <div class="line"></div> */}
-  
-            <div class="quote">
-              <hr class="line" />
-              {generatingQuote ? (
-                <div>
-                  <Typography
-                    style={{ marginLeft: "10px", color: "white" }}
-                    variant="overline"
-                  >
-                    Writing the quote...
-                  </Typography>
-                  <LinearProgress
-                    color="secondary"
-                    style={{ width: "100%" }}
-                    className={classes.progressModel}
-                  />
-                </div>
-              ) : (
-                <p class="text">{quote}</p>
-              )}
-              <p class="attr">{author}</p>
-              <hr class="line" />
-            </div>
-          </section>
+           </div>
+           </div>
         </Grid>
       </Grid>
     ); 
